56f2cc09246574e6060c8093cdf70d4e8f9937ec
Rahul Baskey <86495244+RahulBRB@users.noreply.github.com>
2023-10-30 13:30:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,33 @@ const mode = () => {
     let newbody = document.body;
     newbody.classList.toggle('dark');
 }
+
+
+let x=document.getElementById("out");
+let y=document.getElementById("weather");
+function geolocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition)
+
+    }else{
+        x.innerText="Geolocation Not Supported"
+    }
+}
+
+function showPosition(){
+    console.log(data)
+    let lat=data.coords.latitude;
+    let lon=data.coords.longitude;
+    x.innerText="Latitude is ${lat} and Longitude is ${lon}"
+    const url="https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29";
+    
+
+    fetch(url,{method:"GET"})
+    .then((res)=>res.json())
+    .then((data)=>{
+        console.log(data)
+        let cityName = data.city.name;
+        let temp=data.list[0].temp.day+"°C";
+        y.innerText="Weather in ${cityName} is ${temp}"
+    })
+}
